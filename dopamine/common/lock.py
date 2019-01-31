110e3674b1ad37d43b5a6df3e443a37789c2fb6f
Arthur <aarg@google.com>
2019-01-31 02:13:00 -0500

    Remove unnecessary arguments in lock.

--- dopamine/common/lock.py
+++ dopamine/common/lock.py
@@ -25,7 +25,7 @@ _LOCK_ATTR_NAME = '_lock'
 # TODO(aarg): Add a lock implementation that lets read operation occur at the
 # same time (i.e. less restrictive).
 class LockedClass(object):
-  def __init__(self, *args, **kwargs):
+  def __init__(self):
     if hasattr(self, _LOCK_ATTR_NAME):
       raise ValueError('Object already has a `{}` attribute.')
     setattr(self, _LOCK_ATTR_NAME, threading.Lock())
