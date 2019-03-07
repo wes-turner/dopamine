f3b40da2e3428c12ce80c07e0d2da4df07b74653
Arthur <aarg@google.com>
2019-03-07 17:14:09 -0500

    Add local environment management.

--- dopamine/discrete_domains/run_experiment/run_async_training.py
+++ dopamine/discrete_domains/run_experiment/run_async_training.py
@@ -14,12 +14,34 @@
 # limitations under the License.
 """Runs asynchronous training."""
 
+from dopamine.discrete_domains import atari_lib
 from dopamine.discrete_domains.run_experiment import run_experiment
+from dopamine.utils import threading_utils
 import tensorflow as tf
 
 
+@threading_utils.local_attributes(['_environment'])
 class AsyncRunner(run_experiment.Runner):
   """Defines a train runner for asynchronous training."""
+
+  def __init__(
+      self, base_dir, create_agent_fn,
+      create_environment_fn=atari_lib.create_atari_environment, **kwargs):
+    """Creates an asynchronous runner.
+
+    Args:
+      base_dir: str, the base directory to host all required sub-directories.
+      create_agent_fn: A function that takes as args a Tensorflow session and an
+        environment, and returns an agent.
+      create_environment_fn: A function which receives a problem name and
+        creates a Gym environment for that problem (e.g. an Atari 2600 game).
+      **kwargs: Additional positional arguments.
+    """
+    threading_utils.initialize_local_attributes(
+        self, _environment=create_environment_fn)
+    super(AsyncRunner, self).__init__(
+        base_dir=base_dir, create_agent_fn=create_agent_fn,
+        create_environment_fn=create_environment_fn, **kwargs)
 
   def _initialize_session(self):
     """Creates a tf.Session that supports GPU usage in multiple threads."""
