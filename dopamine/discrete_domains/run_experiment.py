990d550b7a746b444113559896e216c3f42dd3ab
Arthur <aarg@google.com>
2019-03-10 13:48:40 -0400

    Move async trainer to same file as other runners.

--- dopamine/discrete_domains/run_experiment.py
+++ dopamine/discrete_domains/run_experiment.py
@@ -22,14 +22,98 @@ import os
 import sys
 import time
 
+from dopamine.agents.dqn import dqn_agent
+from dopamine.agents.implicit_quantile import implicit_quantile_agent
+from dopamine.agents.rainbow import rainbow_agent
 from dopamine.discrete_domains import atari_lib
 from dopamine.discrete_domains import checkpointer
 from dopamine.discrete_domains import iteration_statistics
 from dopamine.discrete_domains import logger
 
-import gin.tf
 import numpy as np
 import tensorflow as tf
+
+import gin.tf
+
+
+def load_gin_configs(gin_files, gin_bindings):
+  """Loads gin configuration files.
+
+  Args:
+    gin_files: list, of paths to the gin configuration files for this
+      experiment.
+    gin_bindings: list, of gin parameter bindings to override the values in
+      the config files.
+  """
+  gin.parse_config_files_and_bindings(gin_files,
+                                      bindings=gin_bindings,
+                                      skip_unknown=False)
+
+
+@gin.configurable
+def create_agent(sess, environment, agent_name=None, summary_writer=None,
+                 debug_mode=False):
+  """Creates an agent.
+
+  Args:
+    sess: A `tf.Session` object for running associated ops.
+    environment: An Atari 2600 Gym environment.
+    agent_name: str, name of the agent to create.
+    summary_writer: A Tensorflow summary writer to pass to the agent
+      for in-agent training statistics in Tensorboard.
+    debug_mode: bool, whether to output Tensorboard summaries. If set to true,
+      the agent will output in-episode statistics to Tensorboard. Disabled by
+      default as this results in slower training.
+
+  Returns:
+    agent: An RL agent.
+
+  Raises:
+    ValueError: If `agent_name` is not in supported list.
+  """
+  assert agent_name is not None
+  if not debug_mode:
+    summary_writer = None
+  if agent_name == 'dqn':
+    return dqn_agent.DQNAgent(sess, num_actions=environment.action_space.n,
+                              summary_writer=summary_writer)
+  elif agent_name == 'rainbow':
+    return rainbow_agent.RainbowAgent(
+        sess, num_actions=environment.action_space.n,
+        summary_writer=summary_writer)
+  elif agent_name == 'implicit_quantile':
+    return implicit_quantile_agent.ImplicitQuantileAgent(
+        sess, num_actions=environment.action_space.n,
+        summary_writer=summary_writer)
+  else:
+    raise ValueError('Unknown agent: {}'.format(agent_name))
+
+
+@gin.configurable
+def create_runner(base_dir, schedule='continuous_train_and_eval'):
+  """Creates an experiment Runner.
+
+  Args:
+    base_dir: str, base directory for hosting all subdirectories.
+    schedule: string, which type of Runner to use.
+
+  Returns:
+    runner: A `Runner` like object.
+
+  Raises:
+    ValueError: When an unknown schedule is encountered.
+  """
+  assert base_dir is not None
+  # Continuously runs training and evaluation until max num_iterations is hit.
+  if schedule == 'continuous_train_and_eval':
+    return Runner(base_dir, create_agent)
+  # Continuously runs training until max num_iterations is hit.
+  elif schedule == 'continuous_train':
+    return TrainRunner(base_dir, create_agent)
+  elif schedule == 'async_train':
+    return AsyncRunner(base_dir, create_agent)
+  else:
+    raise ValueError('Unknown schedule: {}'.format(schedule))
 
 
 @gin.configurable
@@ -453,3 +537,7 @@ class TrainRunner(Runner):
             tag='Train/AverageReturns', simple_value=average_reward),
     ])
     self._summary_writer.add_summary(summary, iteration)
+
+
+class AsyncRunner(Runner):
+  pass
